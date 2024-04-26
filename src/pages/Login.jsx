a33b90af745b3e2026098749f6d8db01b50d52c8
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-04-26 16:30:40 -0700

    added some css and login page stuff

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom"
 
 export function Login() {
     return <>
+    <label>Username</label>
+    <input type="text"/>
+    <label>Password</label>
+    <input type="password"/>
      <Link to="/homepage" style={{ textDecoration: 'none', color: 'black' }}><button id="login-button" className="btn">Login!</button></Link>
     </>
    }
