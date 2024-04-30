b3c9f99901b97dd1de31bcaf5f8d745ca05c0486
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-04-30 16:45:38 -0700

    111

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -13,7 +13,7 @@ export function HomePage() {
             response => response.json()
         ).then(
         data => {
-            setbackendData(date)
+            setbackendData(data)
         })
     }, [])
 
