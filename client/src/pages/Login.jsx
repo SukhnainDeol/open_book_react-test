e734f7a3bcdc03d35c78afd5991253ddd05865e6
Henry Wesson <80785364+wessonh@users.noreply.github.com>
2024-04-30 21:08:07 -0700

    Update Login.jsx
    added the toggle theme button

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import { ToggleTheme } from "./ToggleTheme" // imports toggle theme file
+
 
 export function Login() {
     return <>
@@ -8,5 +10,7 @@ export function Login() {
     <label>Password</label>
     <input type="password"/>
      <Link to="/homepage" style={{ textDecoration: 'none', color: 'black' }}><button id="login-button" className="btn">Login!</button></Link>
+     <br></br>
+    <ToggleTheme />
     </>
    }
