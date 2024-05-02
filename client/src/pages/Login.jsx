63f2cc3e87dcbf33a3b4e18a24ce6d1cbf8f5d26
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-05-01 21:01:26 -0700

    started working on login user stuff

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export function Login() {
-    return <>
 
-    <label>Username</label>
-    <input type="text"/>
-    <label>Password</label>
-    <input type="password"/>
-     <Link to="/homepage" style={{ textDecoration: 'none', color: 'black' }}><button id="login-button" className="btn">Login!</button></Link>
-     <br></br>
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function Try() {
+
+        console.log(username);
+        console.log(password);
+
+        axios.post('http://localhost:5000/users/', {username: username, password: password})
+    }
+
+
+    return <>
+        <label>Username</label>
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+        <label>Password</label>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        < Link to="#" style={{ textDecoration: 'none', color: 'black' }}><button id="login-button" className="btn" onClick={() => Try()} >Login!</button></Link>
     </>
    }
