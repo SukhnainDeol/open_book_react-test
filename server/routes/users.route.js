900b8270fdc6048183381fbf48f5c9be40dde324
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-05-01 19:27:25 -0700

    removed user2 references from users.route

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 
-let User2 = require('../models/user.model2.js')
+let User = require('../models/user.model.js')
 
 // GET REQUESTS TO GET USERS
 router.route('/').get((request, response) => {
     // mongoose function to find all users
-    User2.find()
+    User.find()
     .then(
         users => response.json(users)) // returns all the users from the database
     .catch(
@@ -17,7 +17,7 @@ router.route('/').post((request, response) => {
     const username = request.body.username;
     const password = request.body.password;
 
-    const newUser = new User2({username, password});
+    const newUser = new User({username, password});
 
     newUser.save()
     .then(() => response.json('User Added!')) // IF USER IS SUCCESSFULLY ADDED
