8ba9ee88013f65b09d0b17a1a9eac4b81c19ce24
Sukhnain Deol <dsukhnain@gmail.com>
2024-05-01 21:44:44 -0700

    completed CRUD methods

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -3,6 +3,17 @@ const Post = require('../models/post.model');
 
 
 // GET REQUESTS
+router.route('/').get(async (request, response) => {
+    try {
+        // get posts & and return them
+        const posts = await Post.find({});
+        return response.status(200).json(posts);
+    } catch (error) {
+        console.log("ERROR:", error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+
 
 // POST REQUESTS
 router.route('/').post(async (request, response) => {
@@ -47,10 +58,58 @@ router.route('/').post(async (request, response) => {
     }
 })
 
+
 // PUT REQUESTS
+router.route('/:id').put(async (request, response) => {
+    try {
+        // check if required fields are filled
+        if (!(
+            request.body.author && request.body.text && request.body.date 
+            && request.body.likes && request.body.likes.count 
+            && request.body.dislikes && request.body.dislikes.count
+        )) {
+            return response.status(400).send({
+                message: "Author, Text, Date, Likes Count, and Dislikes Count fields are required",
+            });
+        }
+        
+        // edit post
+        const id = request.params.id;
+        const post = await Post.findByIdAndUpdate(id, request.body);
+
+        // not found Response
+        if (!post) {
+            return response.status(404).send({message: "Post not found"});
+        }
+
+        // success Response
+        return response.status(200).send({message: "Post Updated Successfully!"});
+    } catch (error) {
+        console.log("ERROR:", error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 
 // DELETE REQUESTS
+router.route('/:id').delete(async (request, response) => {
+    try {
+        // delete post
+        const id = request.params.id;
+        const post = await Post.findByIdAndDelete(id);
+
+        // not found Response
+        if (!post) {
+            return response.status(404).send({message: "Post not found"});
+        }
+
+        // success Response
+        return response.status(200).send({message: "Post Deleted Successfully!"});
+    } catch (error) {
+        console.log("ERROR:", error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 
 
