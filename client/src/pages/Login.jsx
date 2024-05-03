3187a07b669fc89b2898e7569fb1a5b260563913
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-05-03 10:16:10 -0700

    comments

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,8 @@ import axios from "axios"
 
 export function Login() {
 
+    // AUTH FUNCTIONS ------------------------------------------------------------------------------------------------------------------
+    
     const navigate = useNavigate();
 
     const {
