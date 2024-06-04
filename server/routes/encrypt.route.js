a1155ca3116cd2ac8a5c3e1d00dca349b22a941a
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-06-03 17:15:45 -0700

    backend comments for password encryption

--- server/routes/encrypt.route.js
+++ server/routes/encrypt.route.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const En = require('../encryption/secret')
+const En = require('../encryption/secret') // ENCRYPTION FILE
 
 router.route('/').get( async (request, response) => {
     try {
-        const password = request.query.password
-        const newPassword = En.setPass(password)
+        const password = request.query.password // PASSWORD FROM USER
+        const newPassword = En.setPass(password) // ENCRYPTS PASSWORD
         response.setHeader('Content-Type', 'application/json');
-        return response.status(200).json(newPassword);
+        return response.status(200).json(newPassword); // RETURNS ENCRYPTED PASSWORD
     } catch (error) {
         console.log("ERROR:", error.message);
         response.status(500).send({message: error.message});
